84af7794b1a272fd26f2b5583014e3c497f45545
Chengkai Yang <jenson.yang20@gmail.com>
2018-09-23 16:56:59 -0400

    e

--- app/src/main/java/com/example/jenson/cs2340_team24_project/UI/WelcomeActivity.java
+++ app/src/main/java/com/example/jenson/cs2340_team24_project/UI/WelcomeActivity.java
@@ -1,4 +1,14 @@
 package com.example.jenson.cs2340_team24_project.UI;
 
-public class WelcomeActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.jenson.cs2340_team24_project.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
 }
