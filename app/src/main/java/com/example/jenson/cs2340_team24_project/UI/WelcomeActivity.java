49087a1d6688739d544775090de3513ee4eed5f7
Chengkai Yang <jenson.yang20@gmail.com>
2018-09-23 17:23:53 -0400

    e

--- app/src/main/java/com/example/jenson/cs2340_team24_project/UI/WelcomeActivity.java
+++ app/src/main/java/com/example/jenson/cs2340_team24_project/UI/WelcomeActivity.java
@@ -1,14 +1,25 @@
 package com.example.jenson.cs2340_team24_project.UI;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.jenson.cs2340_team24_project.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private static int Timeout = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(WelcomeActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },Timeout);
     }
 }
