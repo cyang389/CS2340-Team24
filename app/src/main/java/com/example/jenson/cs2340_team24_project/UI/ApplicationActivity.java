c9883af171e9df2bdfccbf0cf80c716662680fd0
Chengkai Yang <jenson.yang20@gmail.com>
2018-09-23 22:12:43 -0400

    e

--- app/src/main/java/com/example/jenson/cs2340_team24_project/UI/ApplicationActivity.java
+++ app/src/main/java/com/example/jenson/cs2340_team24_project/UI/ApplicationActivity.java
@@ -1,7 +1,10 @@
 package com.example.jenson.cs2340_team24_project.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.example.jenson.cs2340_team24_project.R;
 
 public class ApplicationActivity extends AppCompatActivity {
@@ -10,5 +13,13 @@ public class ApplicationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
+        Button logoutButton = (Button) findViewById(R.id.button3);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ApplicationActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
