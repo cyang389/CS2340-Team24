ad06a795359e50b5de928fb398629c41aa4cd49d
Chengkai Yang <jenson.yang20@gmail.com>
2018-09-23 21:01:28 -0400

    e

--- app/src/main/java/com/example/jenson/cs2340_team24_project/UI/HomeActivity.java
+++ app/src/main/java/com/example/jenson/cs2340_team24_project/UI/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.jenson.cs2340_team24_project.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.jenson.cs2340_team24_project.LoginActivity;
 import com.example.jenson.cs2340_team24_project.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -10,5 +14,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
