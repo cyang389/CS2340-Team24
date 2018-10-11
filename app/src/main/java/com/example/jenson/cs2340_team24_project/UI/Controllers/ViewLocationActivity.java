bf880dccb40b3e96e2956e9061d0aeea8520f771
Michael Mao <jmao@gatech.edu>
2018-10-10 21:34:38 -0400

    Added viewlocation activity and layout

---
+++ app/src/main/java/com/example/jenson/cs2340_team24_project/UI/Controllers/ViewLocationActivity.java
@@ -0,0 +1,13 @@
+package com.example.jenson.cs2340_team24_project.UI.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_location);
+    }
+}
