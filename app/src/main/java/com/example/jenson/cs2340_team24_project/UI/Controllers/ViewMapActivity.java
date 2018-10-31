a523d4e566f19d4bcdd2b0c98995b03a62645582
Chengkai Yang <jenson.yang20@gmail.com>
2018-10-31 14:41:12 -0400

    map

--- app/src/main/java/com/example/jenson/cs2340_team24_project/UI/Controllers/ViewMapActivity.java
+++ app/src/main/java/com/example/jenson/cs2340_team24_project/UI/Controllers/ViewMapActivity.java
@@ -9,10 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+//import com.example.jenson.cs2340_team24_project.Manifest;
 import com.example.jenson.cs2340_team24_project.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class ViewMapActivity extends AppCompatActivity {
 
@@ -25,6 +30,7 @@ public class ViewMapActivity extends AppCompatActivity {
         if (googleServicesAvailable()) {
             Toast.makeText(this, "Perfect!!", Toast.LENGTH_LONG).show();
         }
+        initMap();
 
         Button back = (Button) findViewById(R.id.mapBackButton);
         back.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +42,15 @@ public class ViewMapActivity extends AppCompatActivity {
         });
     }
 
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mGoogleMap = googleMap;
+            }
+        });
+    }
     public boolean googleServicesAvailable() {
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
