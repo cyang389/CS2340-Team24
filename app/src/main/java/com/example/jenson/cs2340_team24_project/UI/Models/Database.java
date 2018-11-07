4372e4cfdd79b4693747a470b3b2f66cebb739c0
Chengkai Yang <jenson.yang20@gmail.com>
2018-11-07 14:20:30 -0500

    javadoc

--- app/src/main/java/com/example/jenson/cs2340_team24_project/UI/Models/Database.java
+++ app/src/main/java/com/example/jenson/cs2340_team24_project/UI/Models/Database.java
@@ -3,6 +3,9 @@ package com.example.jenson.cs2340_team24_project.UI.Models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Store legal locations
+ */
 public class Database {
     private static final List<String> legalLocations = new ArrayList<>();
 
